329050972aa1e4a6a9d3cac563a0b6eb7a9d5a3d
Ahmed Ghorbel <ahmed.ghorbel888@gmail.com>
2024-07-19 10:35:47 +0200

    first commit

---
+++ model.py
@@ -0,0 +1,350 @@
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchinfo
+from mamba_ssm.modules.mamba_simple import Mamba
+from mamba_ssm.ops.triton.layernorm import RMSNorm
+
+from layers import Attention, Mlp, Gmlp, modulate, get_2d_sincos_pos_embed
+
+
+
+#################################################################################
+#               Embedding Layer for Timesteps                                   #
+#################################################################################
+class TimestepEmbedder(nn.Module):
+    """
+    For denoising timesteps embedding in the final diffusion model
+    Embeds scalar timesteps into vector representations.
+    """
+    def __init__(self, hidden_size, frequency_embedding_size=256):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(frequency_embedding_size, hidden_size, bias=True),
+            nn.SiLU(),
+            nn.Linear(hidden_size, hidden_size, bias=True),
+        )
+        self.frequency_embedding_size = frequency_embedding_size
+
+    @staticmethod
+    def timestep_embedding(t, dim, max_period=1000):
+        """
+        Create sinusoidal timestep embeddings.
+        :param t: a 1-D Tensor of N indices, one per batch element.
+                          These may be fractional.
+        :param dim: the dimension of the output.
+        :param max_period: controls the minimum frequency of the embeddings.
+        :return: an (N, D) Tensor of positional embeddings.
+        """
+        # https://github.com/openai/glide-text2im/blob/main/glide_text2im/nn.py
+        half = dim // 2
+        freqs = torch.exp(
+            -math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32) / half
+        ).to(device=t.device)
+        args = t[:, None].float() * freqs[None]
+        embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
+        if dim % 2:
+            embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
+        return embedding
+
+    def forward(self, t):
+        t_freq = self.timestep_embedding(t, self.frequency_embedding_size)
+        t_emb = self.mlp(t_freq)
+        return t_emb
+
+
+class MapEmbedder(nn.Module):
+    """
+    Map encoding as context to condition the TrafficDiffuser.
+    """
+    def __init__(self, input_size, output_dim):
+        super().__init__()
+        self.h, self.w = input_size[0]//32, input_size[1]//32  # 5 pool layers (downsample)
+        self.conv1 = nn.Conv2d(input_size[2], 64, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.fc1 = nn.Linear(256 * self.h * self.w, 512) 
+        self.fc2 = nn.Linear(512, output_dim)
+
+    def forward(self, x):
+        # (B, C, H, W) Shape of the map from the dataloader after transform
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.pool(F.relu(self.conv5(x)))
+        x = x.view(-1, 256 * self.h * self.w)  # Flatten the tensor
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+
+#################################################################################
+#                       Core TrafficDiffuser Model                              #
+#################################################################################
+class MaskedTransformer(nn.Module):
+    """
+    A Masked Transformer block with adaptive layer norm zero (adaLN-Zero) conditioning
+    """
+    def __init__(self, hidden_size, num_heads, mlp_ratio=4.0):
+        super().__init__()
+        self.num_heads = num_heads
+        self.norm1 = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.attn = Attention(dim=hidden_size, num_heads=num_heads)
+        self.norm2 = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        mlp_hidden_dim = int(hidden_size * mlp_ratio)
+        approx_gelu = lambda: nn.GELU(approximate="tanh")
+        self.mlp = Mlp(in_features=hidden_size, hidden_features=mlp_hidden_dim, act_layer=approx_gelu, drop=0)
+        self.adaLN_modulation = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(hidden_size, 6 * hidden_size, bias=True)
+        )
+
+    def forward(self, x, mask, c):
+        # [(B, N, H), (B, N, H), (B, H)]
+        shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = self.adaLN_modulation(c).chunk(6, dim=1)
+        # Masked attention
+        x = x + gate_msa.unsqueeze(1) * self.attn(modulate(self.norm1(x), shift_msa, scale_msa), mask)  # (B, N, H)
+        x = x * mask                                                                                    # (B, N, H)
+        # Mlp/Gmlp
+        x = x + gate_mlp.unsqueeze(1) * self.mlp(modulate(self.norm2(x), shift_mlp, scale_mlp))         # (B, N, H)
+        x = x * mask                                                                                    # (B, N, H)
+        return x
+
+
+class FinalLayer(nn.Module):
+    """
+    The final layer of the TrafficDiffuser
+    Used for adaLN, project x the to desired output size, and mask the padded agents
+    """
+    def __init__(self, hidden_size, seq_length, dim_size):
+        super().__init__()
+        self.seq_length = seq_length
+        self.dim_size = dim_size
+        self.norm_final = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.linear = nn.Linear(hidden_size, seq_length*dim_size, bias=True)
+        self.adaLN_modulation = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(hidden_size, 2 * hidden_size, bias=True)
+        )
+
+    def forward(self, x, mask, c):
+        # (B, N, H)
+        shift, scale = self.adaLN_modulation(c).chunk(2, dim=1)
+        x = modulate(self.norm_final(x), shift, scale)                      # (B, N, H)
+        x = self.linear(x)                                                  # (B, N, L*D)
+        x = x.view(x.shape[0], x.shape[1], self.seq_length, self.dim_size)  # (B, N, L, D)
+        x = x * mask                                                        # (B, N, L, D)
+        return x
+    
+    
+class TrafficDiffuser(nn.Module):
+    """
+    Diffusion backbone with Transformer layers.
+    """
+    def __init__(
+        self,
+        max_num_agents,
+        seq_length,
+        dim_size,
+        hidden_size,
+        num_heads,
+        depth,
+        map_size=(256, 256, 3),
+        mlp_ratio=4.0,
+        use_ckpt_wrapper=True,
+    ):
+        super().__init__()
+        #self.l_hist = hist_length
+        #self.h_embedder = HistoryEmbedder(hidden_size, num_heads, mlp_ratio=mlp_ratio)
+        self.t_embedder = TimestepEmbedder(hidden_size)
+        self.m_embedder = MapEmbedder(input_size=map_size, output_dim=hidden_size)
+        
+        self.proj1 = nn.Linear(dim_size, hidden_size, bias=True)
+        self.proj2 = nn.Linear(seq_length*hidden_size, hidden_size, bias=True)     
+        #self.proj2 = nn.Linear((seq_length-hist_length)*hidden_size, hidden_size, bias=True)
+        #self.t_pos_embed = nn.Parameter(torch.zeros(1, (seq_length-hist_length), hidden_size), requires_grad=False)
+        self.t_blocks = nn.ModuleList([
+            MaskedTransformer(hidden_size, num_heads, mlp_ratio=mlp_ratio) for _ in range(depth)
+        ])
+        #self.a_pos_embed = nn.Parameter(torch.zeros(1, max_num_agents, hidden_size), requires_grad=False)
+        self.a_blocks = nn.ModuleList([
+            MaskedTransformer(hidden_size, num_heads, mlp_ratio=mlp_ratio) for _ in range(depth)
+        ])
+        self.final_layer = FinalLayer(hidden_size, seq_length, dim_size) 
+        #self.final_layer = FinalLayer(hidden_size, (seq_length-hist_length), dim_size)
+        
+        self.use_ckpt_wrapper = use_ckpt_wrapper
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        # Initialize transformer layers:
+        def _basic_init(module):
+            if isinstance(module, nn.Linear):
+                torch.nn.init.xavier_uniform_(module.weight)
+                if module.bias is not None:
+                    nn.init.constant_(module.bias, 0)
+        self.apply(_basic_init)
+        
+        # Initialize (and freeze) pos_embed by sin-cos embedding:
+        #t_pos_embed = get_2d_sincos_pos_embed(self.t_pos_embed.shape[-1], int(self.seq_length ** 0.5))
+        #self.t_pos_embed.data.copy_(torch.from_numpy(t_pos_embed).float().unsqueeze(0))
+        #a_pos_embed = get_2d_sincos_pos_embed(self.a_pos_embed.shape[-1], int(self.max_num_agents ** 0.5))
+        #self.a_pos_embed.data.copy_(torch.from_numpy(a_pos_embed).float().unsqueeze(0))
+        
+        # Initialize timestep embedding MLP:
+        nn.init.normal_(self.t_embedder.mlp[0].weight, std=0.02)
+        nn.init.normal_(self.t_embedder.mlp[2].weight, std=0.02)
+
+        # Zero-out adaLN modulation layers for t_block:
+        for block in self.t_blocks:
+            nn.init.constant_(block.adaLN_modulation[-1].weight, 0)
+            nn.init.constant_(block.adaLN_modulation[-1].bias, 0)
+        
+        # Zero-out adaLN modulation layers for a_block:
+        for block in self.a_blocks:
+            nn.init.constant_(block.adaLN_modulation[-1].weight, 0)
+            nn.init.constant_(block.adaLN_modulation[-1].bias, 0)
+            
+        # Zero-out output layers:
+        nn.init.constant_(self.final_layer.adaLN_modulation[-1].weight, 0)
+        nn.init.constant_(self.final_layer.adaLN_modulation[-1].bias, 0)
+        nn.init.constant_(self.final_layer.linear.weight, 0)
+        nn.init.constant_(self.final_layer.linear.bias, 0)
+    
+    def ckpt_wrapper(self, module):
+        def ckpt_forward(*inputs):
+            outputs = module(*inputs)
+            return outputs
+        return ckpt_forward
+    
+    def forward(self, x, t, mask, mp):
+        """
+        Forward pass of TrafficDiffuser.
+        - x: (B, N, L, D) tensor of agents where N:max_num_agents, L:sequence_length, and D:sequence_dim
+              sequence_dim is the shape of [valid, x, y, h, Vx, Vy, W, L]
+        - mask: (B, N, L, D) tensor of the mask used to pad to the max agents because of the variable num_agent through different scenarios.
+        - mp: (B, C, H, W) tensor of the map per scenario either png of npy file format
+        - t: (N,) tensor of diffusion timesteps     
+        """
+        #hist = x[:, :, :self.l_hist, :]     # history information to embed as cond 
+        #x = x[:, :, self.l_hist:, :]        # future information
+        
+        ###################### Embedders ############################
+        # (B, t_max=1000)
+        t = self.t_embedder(t)                # (B, H) ts for diff process
+        # (B, H, W, C) or (B, F, 2)
+        mp = self.m_embedder(mp)             # (B, H)
+        #h = self.h_embedder(hist)            # (B, H)
+        #c = t + mp + h                       # (B, H)
+        c = t + mp                           # (B, H)
+        #c = t                                # (B, H)
+        #############################################################
+        
+        
+        ################# Temporal Attention ########################
+        # x is of shape (B, N, L, D) but should be (B*N, L, H)
+        x = self.proj1(x)                                                   # (B, N, L, H)
+        B, N, L, H = x.shape[0], x.shape[1], x.shape[2], x.shape[3]
+        x = x.reshape(B*N, L, H)                                            # (B*N, L, H)
+
+        # mask is of shape (B, N, L, D) but should be (B*N, L, H)
+        msk = mask[:, :, :, 0]                                              # (B, N, L)        
+        msk = msk.reshape(B*N, L)                                           # (B*N, L)
+        msk = msk.unsqueeze(2)                                              # (B*N, L, 1)
+        msk = msk.expand(B*N, L, H).contiguous()                            # (B*N, L, H)
+        
+        # c is of shape (B, H) but should be (B*N, H)
+        ct = c.unsqueeze(1)                                                 # (B, 1, H)
+        ct = ct.expand(B, N, H)                                             # (B, N, H)
+        ct = ct.reshape(B*N, H)                                             # (B*N, H)
+        
+        #x += self.t_pos_embed
+        if self.use_ckpt_wrapper:
+            for block in self.t_blocks:
+                x = torch.utils.checkpoint.checkpoint(self.ckpt_wrapper(block), x, msk, ct, use_reentrant=False)  # (B*N, L, H)
+        else:
+            for block in self.t_blocks:
+                x = block(x, msk, ct)       # (B*N, L, H)
+        
+        x = x.reshape(B, N, L, H)           # (B, N, L, H)       
+        x = x.flatten(2)                    # (B, N, L*H)
+        x = self.proj2(x)                   # (B, N, H)   
+        #############################################################
+        
+        
+        ################### Agent Attention #########################
+        # x is of shape (B, N, H)
+        # c is of shape (B, H)
+        # mask is of shape (B, N, L, D) but should be (B, N, H)
+        msk = mask[:, :, 0, 0]                                                               # (B, N)        
+        msk = msk.unsqueeze(2)                                                               # (B, N, 1)
+        msk = msk.expand(B, N, H).contiguous()                                               # (B, N, H)
+        
+        #x += self.a_pos_embed
+        if self.use_ckpt_wrapper:
+            for block in self.a_blocks:
+                x = torch.utils.checkpoint.checkpoint(self.ckpt_wrapper(block), x, msk, c, use_reentrant=False)   # (B, N, H)
+        else:
+            for block in self.a_blocks:
+                x = block(x, msk, c)        # (B, N, H)
+        #############################################################
+        
+        
+        ##################### Final layer ###########################
+        # x is of shape (B, N, H)
+        x = self.final_layer(x, mask, c)    # (B, N, L, D)
+        #############################################################
+        
+        #x = torch.concat(hist, x, axis=2)
+        return x
+
+'''
+    def forward_with_cfg(self, x, mask, mp, t, cfg_scale):
+        """
+        Forward pass of TrafficDiffuser, but also batches the unconditional forward pass for classifier-free guidance.
+        """
+        # https://github.com/openai/glide-text2im/blob/main/notebooks/text2im.ipynb
+        half = x[: len(x) // 2]
+        combined = torch.cat([half, half], dim=0)
+        model_out = self.forward(combined, t, y)
+        # For exact reproducibility reasons, we apply classifier-free guidance on only
+        # three channels by default. The standard approach to cfg applies it to all channels.
+        # This can be done by uncommenting the following line and commenting-out the line following that.
+        # eps, rest = model_out[:, :self.in_channels], model_out[:, self.in_channels:]
+        eps, rest = model_out[:, :3], model_out[:, 3:]
+        cond_eps, uncond_eps = torch.split(eps, len(eps) // 2, dim=0)
+        half_eps = uncond_eps + cfg_scale * (cond_eps - uncond_eps)
+        eps = torch.cat([half_eps, half_eps], dim=0)
+        return torch.cat([eps, rest], dim=1)
+''' 
+    
+
+#################################################################################
+#                          TrafficDiffuser Configs                              #
+#################################################################################
+def TrafficDiffuser_H(**kwargs):
+    return TrafficDiffuser(hidden_size=512, num_heads=16, depth=28, **kwargs)
+
+def TrafficDiffuser_L(**kwargs):
+    return TrafficDiffuser(hidden_size=256, num_heads=16, depth=22, **kwargs)
+
+def TrafficDiffuser_B(**kwargs):
+    return TrafficDiffuser(hidden_size=128, num_heads=8, depth=16, **kwargs)
+
+def TrafficDiffuser_S(**kwargs):
+    return TrafficDiffuser(hidden_size=64, num_heads=4, depth=8, **kwargs)
+
+
+TrafficDiffuser_models = {
+    'TrafficDiffuser-H': TrafficDiffuser_H,
+    'TrafficDiffuser-L': TrafficDiffuser_L,
+    'TrafficDiffuser-B': TrafficDiffuser_B,
+    'TrafficDiffuser-S': TrafficDiffuser_S,
+}
---
+++ train.py
@@ -0,0 +1,271 @@
+import os
+import argparse
+import logging
+from copy import deepcopy
+from glob import glob
+from time import time
+
+import torch
+import torch
+import torch.nn as nn
+import torch.optim as optim
+# the first flag below was False when we tested this script but True makes A100 training a lot faster:
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.allow_tf32 = True
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+
+import numpy as np
+from collections import OrderedDict
+from accelerate import Accelerator
+from PIL import Image
+
+from model import TrafficDiffuser_models
+from diffusion import create_diffusion
+
+
+
+#################################################################################
+#                             Training Helper Functions                         #
+#################################################################################
+@torch.no_grad()
+def update_ema(ema_model, model, decay=0.9999):
+    """
+    Step the EMA model towards the current model.
+    """
+    ema_params = OrderedDict(ema_model.named_parameters())
+    model_params = OrderedDict(model.named_parameters())
+
+    for name, param in model_params.items():
+        name = name.replace("module.", "")
+        ema_params[name].mul_(decay).add_(param.data, alpha=1 - decay)
+
+
+def requires_grad(model, flag=True):
+    """
+    Set requires_grad flag for all parameters in a model.
+    """
+    for p in model.parameters():
+        p.requires_grad = flag
+
+
+def create_logger(logging_dir):
+    """
+    Create a logger that writes to a log file and stdout.
+    """
+    logging.basicConfig(
+        level=logging.INFO,
+        format='[\033[34m%(asctime)s\033[0m] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        handlers=[logging.StreamHandler(), logging.FileHandler(f"{logging_dir}/log.txt")]
+    )
+    logger = logging.getLogger(__name__)
+    return logger
+
+
+#################################################################################
+#                                Dataloader                                     #
+#################################################################################
+class CustomDataset(Dataset):
+    def __init__(self, data_path, map_path):
+        self.data_path = data_path
+        self.map_path = map_path   
+        self.data_files = sorted(os.listdir(data_path))
+        self.map_files = sorted(os.listdir(map_path))
+        self.transform = transforms.Compose([
+            #transforms.Resize((256, 256)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Using ImageNet statistics
+        ])
+
+    def __len__(self):
+        assert len(self.data_files) == len(self.map_files), \
+            "Number of data files and map files should be same"
+        return len(self.data_files)
+
+    def __getitem__(self, idx):      
+        data_file = self.data_files[idx]
+        map_file = self.map_files[idx]
+
+        data_npy = np.load(os.path.join(self.data_path, data_file))
+        map_rgb = Image.open(os.path.join(self.map_path, map_file)).convert('RGB')
+        
+        return torch.tensor(data_npy, dtype=torch.float32), self.transform(map_rgb)
+    
+    
+# Adapt the max_agents from dataset to other
+def collate_fn(batch, max_agents=234):
+    padded_batch = []
+    masks = []
+    map_images = []
+
+    for x, map_image in batch:
+        pad_n = max_agents - x.size(0)
+        pad_size = (0, 0, 0, 0, 0, pad_n)  # padding only the first dimension
+        padded_x = nn.functional.pad(x, pad_size, "constant", 0.0)
+        padded_batch.append(padded_x)
+
+        # Create a mask for valid agents
+        mask = torch.ones((x.size(0), x.size(1), x.size(2)), dtype=torch.float32)
+        mask = nn.functional.pad(mask, pad_size, "constant", 0.0)
+        masks.append(mask)
+
+        map_images.append(map_image)
+    
+    padded_batch = torch.stack(padded_batch)
+    masks = torch.stack(masks)
+    map_images = torch.stack(map_images)
+
+    return padded_batch, masks, map_images 
+    
+    
+    
+#################################################################################
+#                                  Training Loop                                #
+#################################################################################
+def main(args):
+    """
+    Trains a diffusion model.
+    """
+    assert torch.cuda.is_available(), "Training currently requires at least one GPU."
+
+    # Setup accelerator:
+    accelerator = Accelerator()
+    device = accelerator.device
+
+    # Setup an experiment folder:
+    if accelerator.is_main_process:
+        os.makedirs(args.results_dir, exist_ok=True)  # Make results folder (holds all experiment subfolders)
+        experiment_index = len(glob(f"{args.results_dir}/*"))
+        experiment_dir = f"{args.results_dir}/{experiment_index:03d}-{args.model}"  # Create an experiment folder
+        checkpoint_dir = f"{experiment_dir}/checkpoints"  # Stores saved model checkpoints
+        os.makedirs(checkpoint_dir, exist_ok=True)
+        logger = create_logger(experiment_dir)
+        logger.info(f"Experiment directory created at {experiment_dir}")
+
+    # Create model:
+    model = TrafficDiffuser_models[args.model](
+        max_num_agents=args.max_agents,
+        seq_length=args.seq_length,
+        dim_size=args.dim_size,
+    )
+    
+    # Note that parameter initialization is done within the model constructor
+    model = model.to(device)
+    ema = deepcopy(model).to(device)  # Create an EMA of the model for use after training
+    requires_grad(ema, False)
+    diffusion = create_diffusion(timestep_respacing="")  # default: 1000 steps, linear noise schedule
+    if accelerator.is_main_process:
+        logger.info(f"{args.model} Parameters: {sum(p.numel() for p in model.parameters()):,}")
+
+    # Setup optimizer:
+    opt = optim.AdamW(model.parameters(), lr=1e-4, weight_decay=0)
+
+    # Setup data:
+    # Dataloader
+    # The dataloader will return a batch of shape [data, mask, map]
+    # of shape [(batch_size, max_agents, seq_length, dim), (batch_size, max_agents, seq_length, dim), (batch_size, H, W, C)]
+    dataset = CustomDataset(data_path=args.data_path, map_path=args.map_path)
+    loader = DataLoader(
+        dataset,
+        batch_size=int(args.global_batch_size // accelerator.num_processes),
+        shuffle=True,
+        collate_fn=collate_fn,
+        num_workers=args.num_workers,
+        pin_memory=True,
+        drop_last=True,
+    )
+    if accelerator.is_main_process:
+        logger.info(f"Dataset contains {len(dataset):,} scenarios ({args.data_path})")
+    
+    # Prepare models for training:
+    update_ema(ema, model, decay=0)  # Ensure EMA is initialized with synced weights
+    model.train()  # important! This enables embedding dropout for classifier-free guidance
+    ema.eval()  # EMA model should always be in eval mode
+    model, opt, loader = accelerator.prepare(model, opt, loader)
+
+    # Variables for monitoring/logging purposes:
+    train_steps = 0
+    log_steps = 0
+    running_loss = 0
+    start_time = time()
+    
+    if accelerator.is_main_process:
+        logger.info(f"Training for {args.epochs} epochs...")
+    for epoch in range(args.epochs):
+        if accelerator.is_main_process:
+            logger.info(f"Beginning epoch {epoch}...")
+        for x, mask, mp in loader:
+            x = x.to(device)
+            mask = mask.to(device)
+            mp = mp.to(device)
+            x = x.squeeze(dim=1)
+            mask = mask.squeeze(dim=1)
+            mp = mp.squeeze(dim=1)
+            t = torch.randint(0, diffusion.num_timesteps, (x.shape[0],), device=device)
+            model_kwargs = dict(mask=mask, mp=mp)
+            loss_dict = diffusion.training_losses(model, x, t, model_kwargs)
+            loss = loss_dict["loss"].mean()
+            opt.zero_grad()
+            accelerator.backward(loss)
+            opt.step()
+            update_ema(ema, model)
+
+            # Log loss values:
+            running_loss += loss.item()
+            log_steps += 1
+            train_steps += 1
+            if train_steps % args.log_every == 0:
+                # Measure training speed:
+                torch.cuda.synchronize()
+                end_time = time()
+                steps_per_sec = log_steps / (end_time - start_time)
+                # Reduce loss history over all processes:
+                avg_loss = torch.tensor(running_loss / log_steps, device=device)
+                avg_loss = avg_loss.item() / accelerator.num_processes
+                if accelerator.is_main_process:
+                    logger.info(f"(step={train_steps:07d}) Train Loss: {avg_loss:.4f}, Train Steps/Sec: {steps_per_sec:.2f}")
+                # Reset monitoring variables:
+                running_loss = 0
+                log_steps = 0
+                start_time = time()
+
+            # Save model checkpoint:
+            if train_steps % args.ckpt_every == 0 and train_steps > 0:
+                if accelerator.is_main_process:
+                    checkpoint = {
+                        "model": model.module.state_dict(),
+                        "ema": ema.state_dict(),
+                        "opt": opt.state_dict(),
+                        "args": args
+                    }
+                    checkpoint_path = f"{checkpoint_dir}/{train_steps:07d}.pt"
+                    torch.save(checkpoint, checkpoint_path)
+                    logger.info(f"Saved checkpoint to {checkpoint_path}")
+
+    model.eval()  # important! This disables randomized embedding dropout
+    
+    if accelerator.is_main_process:
+        logger.info("Done!")
+
+
+# To launch TrafficDiffuser-S training with multiple GPUs on one node:
+# accelerate launch --multi_gpu train.py
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data-path", type=str, default="/data/tii/data/nuscenes_trainval_npy")
+    parser.add_argument("--map-path", type=str, default="/data/tii/data/nuscenes_maps/nuscenes_trainval_maps_png1")
+    parser.add_argument("--results-dir", type=str, default="results")
+    parser.add_argument("--model", type=str, choices=list(TrafficDiffuser_models.keys()), default="TrafficDiffuser-S")
+    parser.add_argument("--max-agents", type=int, default=234)
+    parser.add_argument("--seq-length", type=int, default=156)
+    parser.add_argument("--dim-size", type=int, default=8)
+    parser.add_argument("--epochs", type=int, default=1000)
+    parser.add_argument("--global-batch-size", type=int, default=64)
+    parser.add_argument("--global-seed", type=int, default=0)
+    parser.add_argument("--num-workers", type=int, default=4)
+    parser.add_argument("--log-every", type=int, default=13)
+    parser.add_argument("--ckpt-every", type=int, default=1300)
+    args = parser.parse_args()
+    main(args)
