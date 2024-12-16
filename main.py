2e28eb7a6f0065b9a5c5f285a76ba497ef0f09d9
Ahmed Ghorbel <ahmed.ghorbel888@gmail.com>
2024-12-16 15:53:15 +0100

    update main

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import os
 if __name__ == '__main__':
     
     # Train on multiple gpus
-    os.system("accelerate launch -m scripts.train --config configs/config_train.yaml")
+    os.system("accelerate launch -m scripts.train_wmp --config configs/config_train2.yaml")
+    
+    os.system("accelerate launch --main_process_port 29502 -m scripts.train --config configs/config_train1.yaml")
 
     # Train on 1 gpu
-    #os.system("accelerate launch --num-processes=1 --gpu_ids 1 -m scripts.train --config configs/config_train.yaml")
+    os.system("accelerate launch --num-processes=1 --gpu_ids 1 -m scripts.train --config configs/config_train.yaml")
 
     # Sample
     os.system("python -m scripts.sample --config configs/config_sample.yaml")
