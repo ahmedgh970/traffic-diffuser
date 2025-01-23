50f3f2073c0e516af9f48904477f80975fb3e850
Ahmed Ghorbel <ahmed.ghorbel888@gmail.com>
2025-01-23 15:25:57 +0100

    clean unused classes

--- models/backbones/layers.py
+++ models/backbones/layers.py
@@ -5,182 +5,14 @@ import torch.nn.functional as F
 import numpy as np
 from timm.layers import to_2tuple
 from einops import rearrange, repeat
+from timm.layers import Mlp, GluMlp, GatedMlp      
     
-class SpatialSoftmax(nn.Module):
-    """
-    Spatial Softmax Layer.
-    Based on Deep Spatial Autoencoders for Visuomotor Learning by Finn et al.
-    https://rll.berkeley.edu/dsae/dsae.pdf
-    """
-
-    def __init__(
-            self,
-            input_shape,
-            num_kp=None,
-            temperature=1.,
-            learnable_temperature=False,
-            output_variance=False,
-            noise_std=0.0,
-    ):
-        """
-        Args:
-            input_shape (list, tuple): shape of the input feature (C, H, W)
-            num_kp (int): number of keypoints (None for not use spatialsoftmax)
-            temperature (float): temperature term for the softmax.
-            learnable_temperature (bool): whether to learn the temperature
-            output_variance (bool): treat attention as a distribution, and compute second-order statistics to return
-            noise_std (float): add random spatial noise to the predicted keypoints
-        """
-        super(SpatialSoftmax, self).__init__()
-        assert len(input_shape) == 3
-        self._in_c, self._in_h, self._in_w = input_shape  # (C, H, W)
-
-        if num_kp is not None:
-            self.nets = torch.nn.Conv2d(self._in_c, num_kp, kernel_size=1)
-            self._num_kp = num_kp
-        else:
-            self.nets = None
-            self._num_kp = self._in_c
-        self.learnable_temperature = learnable_temperature
-        self.output_variance = output_variance
-        self.noise_std = noise_std
-
-        if self.learnable_temperature:
-            # temperature will be learned
-            temperature = torch.nn.Parameter(
-                torch.ones(1) * temperature, requires_grad=True)
-            self.register_parameter('temperature', temperature)
-        else:
-            # temperature held constant after initialization
-            temperature = torch.nn.Parameter(
-                torch.ones(1) * temperature, requires_grad=False)
-            self.register_buffer('temperature', temperature)
-
-        pos_x, pos_y = np.meshgrid(
-            np.linspace(-1., 1., self._in_w),
-            np.linspace(-1., 1., self._in_h)
-        )
-        pos_x = torch.from_numpy(pos_x.reshape(
-            1, self._in_h * self._in_w)).float()
-        pos_y = torch.from_numpy(pos_y.reshape(
-            1, self._in_h * self._in_w)).float()
-        self.register_buffer('pos_x', pos_x)
-        self.register_buffer('pos_y', pos_y)
-
-        self.kps = None
-
-    def __repr__(self):
-        """Pretty print network."""
-        header = format(str(self.__class__.__name__))
-        return header + '(num_kp={}, temperature={}, noise={})'.format(
-            self._num_kp, self.temperature.item(), self.noise_std)
-
-    def output_shape(self, input_shape):
-        """
-        Function to compute output shape from inputs to this module.
-        Args:
-            input_shape (iterable of int): shape of input. Does not include batch dimension.
-                Some modules may not need this argument, if their output does not depend
-                on the size of the input, or if they assume fixed size input.
-        Returns:
-            out_shape ([int]): list of integers corresponding to output shape
-        """
-        assert(len(input_shape) == 3)
-        assert(input_shape[0] == self._in_c)
-        return [self._num_kp, 2]
-
-    def forward(self, feature):
-        """
-        Forward pass through spatial softmax layer. For each keypoint, a 2D spatial
-        probability distribution is created using a softmax, where the support is the
-        pixel locations. This distribution is used to compute the expected value of
-        the pixel location, which becomes a keypoint of dimension 2. K such keypoints
-        are created.
-        Returns:
-            out (torch.Tensor or tuple): mean keypoints of shape [B, K, 2], and possibly
-                keypoint variance of shape [B, K, 2, 2] corresponding to the covariance
-                under the 2D spatial softmax distribution
-        """
-        assert(feature.shape[1] == self._in_c)
-        assert(feature.shape[2] == self._in_h)
-        assert(feature.shape[3] == self._in_w)
-        if self.nets is not None:
-            feature = self.nets(feature)
-
-        # [B, K, H, W] -> [B * K, H * W] where K is number of keypoints
-        feature = feature.reshape(-1, self._in_h * self._in_w)
-        # 2d softmax normalization
-        attention = F.softmax(feature / self.temperature, dim=-1)
-        # [1, H * W] x [B * K, H * W] -> [B * K, 1] for spatial coordinate mean in x and y dimensions
-        expected_x = torch.sum(self.pos_x * attention, dim=1, keepdim=True)
-        expected_y = torch.sum(self.pos_y * attention, dim=1, keepdim=True)
-        # stack to [B * K, 2]
-        expected_xy = torch.cat([expected_x, expected_y], 1)
-        # reshape to [B, K, 2]
-        feature_keypoints = expected_xy.view(-1, self._num_kp, 2)
-
-        if self.training:
-            noise = torch.randn_like(feature_keypoints) * self.noise_std
-            feature_keypoints += noise
-
-        if self.output_variance:
-            # treat attention as a distribution, and compute second-order statistics to return
-            expected_xx = torch.sum(
-                self.pos_x * self.pos_x * attention, dim=1, keepdim=True)
-            expected_yy = torch.sum(
-                self.pos_y * self.pos_y * attention, dim=1, keepdim=True)
-            expected_xy = torch.sum(
-                self.pos_x * self.pos_y * attention, dim=1, keepdim=True)
-            var_x = expected_xx - expected_x * expected_x
-            var_y = expected_yy - expected_y * expected_y
-            var_xy = expected_xy - expected_x * expected_y
-            # stack to [B * K, 4] and then reshape to [B, K, 2, 2] where last 2 dims are covariance matrix
-            feature_covar = torch.cat(
-                [var_x, var_xy, var_xy, var_y], 1).reshape(-1, self._num_kp, 2, 2)
-            feature_keypoints = (feature_keypoints, feature_covar)
-
-        if isinstance(feature_keypoints, tuple):
-            self.kps = (feature_keypoints[0].detach(),
-                        feature_keypoints[1].detach())
-        else:
-            self.kps = feature_keypoints.detach()
-        return feature_keypoints
-
-class AdaTemporalEnc(nn.Module):
-    """
-    A Transformer encoder block with adaptive layer norm zero (adaLN-Zero) conditioning
-    """
-    def __init__(self, hidden_size, num_heads, mlp_ratio=4.0):
-        super().__init__()
-        self.num_heads = num_heads
-        self.norm1 = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
-        self.mha = nn.MultiheadAttention(embed_dim=hidden_size, num_heads=num_heads, batch_first=True)
-        self.norm2 = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
-        mlp_hidden_dim = int(hidden_size * mlp_ratio)
-        approx_gelu = lambda: nn.GELU(approximate="tanh")
-        self.mlp = Mlp(in_features=hidden_size, hidden_features=mlp_hidden_dim, act_layer=approx_gelu, drop=0)
-        self.adaLN_modulation = nn.Sequential(
-            nn.SiLU(),
-            nn.Linear(hidden_size, 6 * hidden_size, bias=True)
-        )
-
-    def forward(self, x, c, mask=None):
-        # [(B, N, H), (B, H)]
-        shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = self.adaLN_modulation(c).chunk(6, dim=1)
-        # Attention
-        x_mod = modulate(self.norm1(x), shift_msa, scale_msa)           # (B, N, H)
-        x_mod, _ = self.mha(x_mod, x_mod, x_mod, key_padding_mask=mask) # (B, N, H)
-        x = x + gate_msa.unsqueeze(1) * x_mod                           # (B, N, H)
-        # Mlp/Gmlp
-        x = x + gate_mlp.unsqueeze(1) * self.mlp(modulate(self.norm2(x), shift_mlp, scale_mlp))               # (B, N, H)
-        return x
     
     
 def modulate(x, shift, scale):
     # x: (B, N, H), shift: (B, H), scale: (B, H)
     # Broadcasted addition and multiplication
     return x * (1 + scale.unsqueeze(1)) + shift.unsqueeze(1)   # (B, N, H) 
-
 
 class AdaTransformerDec(nn.Module):
     """
@@ -215,7 +47,33 @@ class AdaTransformerDec(nn.Module):
         # Mlp/Gmlp
         x = x + gate_mlp.unsqueeze(1) * self.mlp(modulate(self.norm3(x), shift_mlp, scale_mlp))                   # (B*N, L, H)
         return x
-        
+  
+class AdaTransformerEnc(nn.Module):
+    """
+    A Transformer encoder block with adaptive layer norm zero (adaLN-Zero) conditioning
+    """
+    def __init__(self, hidden_size, num_heads, mlp_ratio=4.0):
+        super().__init__()
+        self.num_heads = num_heads
+        self.norm1 = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.attn = Attention(dim=hidden_size, num_heads=num_heads)
+        self.norm2 = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        mlp_hidden_dim = int(hidden_size * mlp_ratio)
+        approx_gelu = lambda: nn.GELU(approximate="tanh")
+        self.mlp = Mlp(in_features=hidden_size, hidden_features=mlp_hidden_dim, act_layer=approx_gelu, drop=0)
+        self.adaLN_modulation = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(hidden_size, 6 * hidden_size, bias=True)
+        )
+
+    def forward(self, x, c, mask=None):
+        # [(B, N, H), (B, H)]
+        shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = self.adaLN_modulation(c).chunk(6, dim=1)
+        # Attention
+        x = x + gate_msa.unsqueeze(1) * self.attn(modulate(self.norm1(x), shift_msa, scale_msa), mask=mask)   # (B, N, H)
+        # Mlp/Gmlp
+        x = x + gate_mlp.unsqueeze(1) * self.mlp(modulate(self.norm2(x), shift_mlp, scale_mlp))               # (B, N, H)
+        return x
 
 class CrossAttentionLayer(nn.Module):
     """
@@ -289,36 +147,7 @@ class CrossAttentionLayer(nn.Module):
         # Final output projection
         output = self.out_proj(attended)  # (B, Q_len, embed_dim)
         return output
-
-  
-class AdaTransformerEnc(nn.Module):
-    """
-    A Transformer encoder block with adaptive layer norm zero (adaLN-Zero) conditioning
-    """
-    def __init__(self, hidden_size, num_heads, mlp_ratio=4.0):
-        super().__init__()
-        self.num_heads = num_heads
-        self.norm1 = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
-        self.attn = Attention(dim=hidden_size, num_heads=num_heads)
-        self.norm2 = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
-        mlp_hidden_dim = int(hidden_size * mlp_ratio)
-        approx_gelu = lambda: nn.GELU(approximate="tanh")
-        self.mlp = Mlp(in_features=hidden_size, hidden_features=mlp_hidden_dim, act_layer=approx_gelu, drop=0)
-        self.adaLN_modulation = nn.Sequential(
-            nn.SiLU(),
-            nn.Linear(hidden_size, 6 * hidden_size, bias=True)
-        )
-
-    def forward(self, x, c, mask=None):
-        # [(B, N, H), (B, H)]
-        shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = self.adaLN_modulation(c).chunk(6, dim=1)
-        # Attention
-        x = x + gate_msa.unsqueeze(1) * self.attn(modulate(self.norm1(x), shift_msa, scale_msa), mask=mask)   # (B, N, H)
-        # Mlp/Gmlp
-        x = x + gate_mlp.unsqueeze(1) * self.mlp(modulate(self.norm2(x), shift_mlp, scale_mlp))               # (B, N, H)
-        return x
-
-
+    
 class Attention(nn.Module):    
     def __init__(
             self,
@@ -364,44 +193,6 @@ class Attention(nn.Module):
         x = self.proj(x)
         x = self.proj_drop(x)
         return x 
-
-
-class Mlp(nn.Module):
-    """ MLP as used in Vision  Transformer, MLP-Mixer and related networks
-    """
-    def __init__(
-            self,
-            in_features,
-            hidden_features=None,
-            out_features=None,
-            act_layer=nn.GELU,
-            norm_layer=None,
-            bias=True,
-            drop=0.,
-            use_conv=False,
-    ):
-        super().__init__()
-        out_features = out_features or in_features
-        hidden_features = hidden_features or in_features
-        bias = to_2tuple(bias)
-        drop_probs = to_2tuple(drop)
-        linear_layer = partial(nn.Conv2d, kernel_size=1) if use_conv else nn.Linear
-
-        self.fc1 = linear_layer(in_features, hidden_features, bias=bias[0])
-        self.act = act_layer()
-        self.drop1 = nn.Dropout(drop_probs[0])
-        self.norm = norm_layer(hidden_features) if norm_layer is not None else nn.Identity()
-        self.fc2 = linear_layer(hidden_features, out_features, bias=bias[1])
-        self.drop2 = nn.Dropout(drop_probs[1])
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.act(x)
-        x = self.drop1(x)
-        x = self.norm(x)
-        x = self.fc2(x)
-        x = self.drop2(x)
-        return x
 
 
 def get_1d_sincos_pos_embed(embed_dim, pos):
